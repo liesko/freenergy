4afc3ae095efdb5ff2169f813d3dd7aa6fc1cdd1
liesko <anton.lieskovsky@gmail.com>
2026-04-02 21:12:16 +0200

    change from localhost to VARIABLE2

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,15 +5,24 @@ import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
+  console.log('FRONTEND_URL:', process.env.FRONTEND_URL);
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
 
   // Security
   app.use(helmet());
   app.use(cookieParser());
+  const frontendUrl = process.env.FRONTEND_URL;
+
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: [
+      frontendUrl,
+      'https://eloquent-gentleness-production-53eb.up.railway.app',
+      'http://localhost:3000'
+    ].filter(Boolean) as string[],
     credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
   // Global Validation
